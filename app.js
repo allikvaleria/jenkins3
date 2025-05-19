5a60743a14db63c3ddc0606ad62fd57254c2034d
Gagagagagcfhsregfbyudsiwfgowse98gvuj <154507539+allikvaleria@users.noreply.github.com>
2025-05-19 14:37:03 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/endpoint', (req, res) => {
+app.get('/travel', (req, res) => {
   res.send('Моё любимое место для путешествий — Япония.');
 });
 
